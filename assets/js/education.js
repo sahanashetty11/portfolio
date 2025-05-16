525f60197f3384afa5be28ddf829f4c8dac1f3a4
Sahana Shetty <sahanashetty9711@gmail.com>
2025-05-16 04:42:23 -0400

    Latest Changes

--- assets/js/education.js
+++ assets/js/education.js
@@ -1,71 +1,5 @@
 AOS.init();
 
-// MOOCs Cards
-
-const moocs = document.querySelector(".moocs");
-const moocscards = [
-  {
-    title: "Social Networks",
-    cardImage: "assets/images/education-page/nptel2.png",
-    moocLink: "https://onlinecourses.nptel.ac.in/noc20_cs78/preview",
-  },
-  {
-    title: "IBM Data Science Specialization",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink:
-      "https://www.coursera.org/account/accomplishments/specialization/certificate/AT82J3B9HKKJ",
-  },
-  {
-    title: "Data Science for Engineers",
-    cardImage: "assets/images/education-page/nptel2.png",
-    moocLink: "https://onlinecourses.nptel.ac.in/noc21_cs23/preview",
-  },
-  {
-    title: "The Complete Android Oreo Developer Course",
-    cardImage: "assets/images/education-page/udemy.png",
-    moocLink:
-      "https://www.udemy.com/course/the-complete-android-oreo-developer-course/",
-  },
-  {
-    title: "Deep Learning Specialization",
-    cardImage: "assets/images/education-page/coursera1.png",
-    moocLink:
-      "https://www.coursera.org/account/accomplishments/specialization/certificate/W4DYPGVJJSYU",
-  },
-  {
-    title: "Python for Data Science",
-    cardImage: "assets/images/education-page/nptel2.png",
-    moocLink: "https://onlinecourses.nptel.ac.in/noc21_cs33/preview",
-  },
-  {
-    title: "Finance for Engineers",
-    cardImage: "assets/images/education-page/emory.jpeg",
-    moocLink: "https://drive.google.com/file/d/15oxLdzUd7Fk0TLWVUz0FPtnoraymbE7F/view?usp=drivesdk",
-  },
-  {
-    title: "Prompt Engineering",
-    cardImage: "assets/images/education-page/vanderbilt.jpeg",
-    moocLink: "https://www.coursera.org/account/accomplishments/verify/VGKWVJAG2WRQ",
-  },
-];
-
-const experience = [
-  {
-    img: "assets/images/education-page/c1.png",
-  },
-  {
-    img: "assets/images/education-page/c2.jpg",
-  },
-  {
-    img: "assets/images/education-page/c3.png",
-  },
-  {
-    img: "assets/images/education-page/c4.png",
-  },
-  {
-    img: "assets/images/education-page/c5.jpg",
-  },
-];
 
 let currentItem = 0;
 
@@ -74,81 +8,8 @@ const img = document.getElementById("image");
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
-window.addEventListener("DOMContentLoaded", function () {
-  showExperience();
-});
 
-function showExperience() {
-  setInterval(function () {
-    if (currentItem === experience.length) {
-      currentItem = 0;
-    }
-    const item = experience[currentItem];
-    img.src = item.img;
-    currentItem++;
-  }, 3000);
-}
 
-const showCards = () => {
-  let output = "";
-  moocscards.forEach(
-    ({ title, cardImage, moocLink }) =>
-      (output += `        
-        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
-            <div class="card mb-3 mx-auto">
-               <div class="content"><a href="${moocLink}"/>
-                  <div class="content-overlay"></div>
-                    <img src=${cardImage} class="card-img-top content-image">     
-                  <div class="content-details fadeIn-bottom">
-                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
-                  </div>
-                </div>
-                <div class="card-body">
-                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
-                </div>
-            </div>
-        </div>        
-      `)
-  );
-  moocs.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards);
-
-/* Badges*/
-
-const bagdes = document.querySelector(".badges");
-const badgesection = [
-  {
-    title: "Google Developer Essentials",
-    image: "assets/images/education-page/badge1.png",
-    description: "Earned May 20, 2020",
-  },
-  {
-    title: "VM Migration",
-    image: "assets/images/education-page/badge2.png",
-    description: "Earned June 20, 2020",
-  },
-  {
-    title: "G Suite Essentials",
-    image: "assets/images/education-page/badge3.png",
-    description: "Earned July 20, 2020",
-  },
-];
-
-const showCards1 = () => {
-  let output = "";
-  badgesection.forEach(
-    ({ title, image, description }) =>
-      (output += `       
-      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
-        <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
-          <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
-          <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
-      </div>`)
-  );
-  bagdes.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards1);
 
 /* Timeline Section*/
 
@@ -159,23 +20,15 @@ const timelinesection = [
     image: "assets/images/education-page/umass.png",
     subheading: "Masters of Science, Computer Science, (2023-Present) ",
     description:
-      "<li>CGPA: 3.95/4</li><li>Machine Learning, Secure Distributed Systems, Systems for Data Science, Software Engineering, Statistics, Database Design and Implementation, Business Intelligence and Analytics, Algorithms for Data Science</li>",
+      "<li>CGPA: 4/4</li><li>Distributed and Operating Systems, Database Design and Implementation, Artificial Intelligence</li>",
   },
   {
-    heading: "SSN College of Engineering",
-    image: "assets/images/education-page/ssn.png",
+    heading: "Ramaiah Institute of Technology",
+    image: "assets/images/education-page/rit.png",
     subheading: "Bachelors of Technology, Information Technology, (2018-2022) ",
     description:
-      "<li>CGPA: 8.9/10</li><li>Event Head of Procode(Intra department coding club)</li><li>Member of Coding Club, ML Tech Club</li>",
-  },
-
-  {
-    heading: "Sacred Heart MHSS Church Park",
-    image: "assets/images/education-page/school.jpeg",
-    subheading: "Computer Science, (2004-2018) ",
-    description:
-      "<br><li>93.75% in HSC</li>&nbsp;&nbsp;&nbsp;<li>97% in SSLC</li><li>Vice Captain and Captain of School house 2016-2018</li><li>Merit Rank Holder from 2008-2018</li><li>Member of Student Council</li>",
-  },
+      "<li>CGPA: 9.43/10</li><li>Data Structures, Machine Learning, Data Mining, Distributed Storage Technologies, Operating Systems etc</li><li>Member of Coding Club, Debugging Club</li>",
+  }
 ];
 
 const showCards2 = () => {
