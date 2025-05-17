1c7af39f6c2149ca7073d8f5bafbf4a6499a6e50
Sahana Shetty <sahanashetty9711@gmail.com>
2025-05-16 22:00:49 -0400

    Image upload

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -16,17 +16,17 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/dp.svg";
+      favicon.href = "./assets/images/folded.png";
     }
   });
 
   function toggle() {
     if (document.title === attentionMessage) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/dp.svg";
+      favicon.href = "./assets/images/folded.png";
     } else {
       document.title = attentionMessage;
-      favicon.href = "./assets/images/folded.png";
+      favicon.href = "./assets/images/dp.png";
     }
   }
 };
